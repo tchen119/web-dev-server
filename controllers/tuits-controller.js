3938ed0c7ab2e862c90bcc7d1002276f86aef438
Tina C <tinac@Tinas-MacBook-Air.local>
2022-03-30 03:04:32 -0400

    first commit

---
+++ controllers/tuits-controller.js
@@ -0,0 +1,41 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {
+  const newTuit = req.body;
+  newTuit._id = (new Date()).getTime()+'';
+  newTuit.stats = {
+    retuits: 111,
+    likes: 222,
+    replies: 333,
+    dislikes: 123
+  };
+  newTuit.postedBy = {
+    "username": "ReactJS"
+  }
+  tuits.push(newTuit);
+  res.json(newTuit);
+}
+
+const findAllTuits = (req, res) =>
+  res.json(tuits);
+
+const updateTuit = (req, res) => {
+  const tuitdIdToUpdate = req.params.tid;
+  const updatedTuit = req.body;
+  tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
+  res.sendStatus(200);
+}
+
+const deleteTuit = (req, res) => {
+  const tuitdIdToDelete = req.params.tid;
+  tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
+  res.sendStatus(200);
+}
+
+export default (app) => {
+  app.post('/api/tuits', createTuit);
+  app.get('/api/tuits', findAllTuits);
+  app.put('/api/tuits/:tid', updateTuit);
+  app.delete('/api/tuits/:tid', deleteTuit);
+}
